36c3519dcf4f88e518131403f1d0495ccb0dc61c
Ahmed Shaikh <268ahm@gmail.com>
2026-04-18 03:53:56 +0500

    first commit

---
+++ src/components/Services.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import assets from '../assets/assets'
+import Title from './title'
+import ServiceCard from './serviceCard'
+import { motion, stagger } from 'motion/react'
+const Services = () => {
+    const servicesData = [
+        {
+            title: 'Advertising',
+            description:'we turn bold ideas into powerful degital solutions that connect,engage...',
+            icon: assets.ads_icon,
+        },
+        {
+            title: 'Content Marketing',
+            description :'we help you execite your plan and deliver results...',
+            icon: assets.marketing_icon,
+        },
+        {
+            title: 'Content Writing',
+            description:'we turn bold ideas into powerful degital solutions that connect,engage...',
+            icon: assets.content_icon,
+        },
+        {
+            title: 'Social Media',
+            description:'we help you execite your plan and deliver results...',
+            icon: assets.social_icon,
+        },
+    ]
+    
+  return (
+    <motion.div
+    initial="hidden"
+    whileInView="visible"
+    viewport={{once:true}}
+    transition={{staggerChildren: 0.2}}
+     id='services' className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-40 text-gray-700 dark:text-white'>
+    <img src={assets.bgImage2} alt="" className='absolute -top-110 -left-70 -z-1 dark:hidden'/>
+    <Title title='How can we help?' desc='From strategy to execeution, we craft digital solutions that move your business forward.'/>
+    <div className='flex flex-col md:grid grid-cols-2'>
+        {servicesData.map((service, index)=>(
+            <ServiceCard key={index} service={service} index={index}/>
+        ))}
+    </div>
+    </motion.div>
+  )
+}
+export default Services
