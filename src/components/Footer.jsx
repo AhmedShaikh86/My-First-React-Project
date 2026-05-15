57cd0f9c76a3fb4f14313a26db29402b016929e8
Ahmed Shaikh <268ahm@gmail.com>
2026-05-16 01:56:46 +0500

    ourwork center and home link fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = ({ theme }) => {
           className='space-y-5 text-sm text-gray-700 dark:text-gray-400'>
           <img src={theme === 'dark' ? assets.logo_dark : assets.logo} className='w-32 sm:w-44' alt="" /><p className='max-w-md'>From strategy to execution, we craft digital solutions that move your business forward.</p>
           <ul className='flex gap-8'>
-            <li><a className='hover:text-primary' href="#home">Home</a></li>
+            <li><a className='hover:text-primary' href="#hero">Home</a></li>
             <li><a className='hover:text-primary' href="#services">Services</a></li>
             <li><a className='hover:text-primary' href="#our-work">Our Work</a></li>
             <li><a className='hover:text-primary' href="#contact-us">Contact Us</a></li>
